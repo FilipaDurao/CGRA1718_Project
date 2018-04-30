fab0069ef49a6520e3d4833e962eee88f72c92d5
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-04-30 17:13:37 +0100

    Added MyTerrain

---
+++ code/MyTerrain.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+class MyTerrain extends Plane {
+    constructor(scene) {
+        super(scene, 50, 0, 1, 0, 1);
+    }
+
+    display() {
+        this.scene.pushMatrix();
+            this.scene.rotate(Math.PI/2, 0, 1, 0);
+            this.scene.scale(50, 1, 50);
+        this.scene.popMatrix();
+    }
+}
